853ea44235cb26b1bf632cd60620b63f52dbdd76
daniel rangel <dr@daniels-MacBook-Pro.local>
2021-05-29 17:32:03 -0400

    first deployment

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('', reviewsRouter);
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
+    app.use(express.static('frontend/build'));
 }
 
 app.listen(`${process.env.PORT}`)
