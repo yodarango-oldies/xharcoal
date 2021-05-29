662fbfd93e45ed7d380837dee0c0cf6e53755139
daniel rangel <dr@daniels-MacBook-Pro.local>
2021-05-29 17:45:06 -0400

    modified proxy to heroku domain

--- frontend/src/components/header-footer/Header.js
+++ frontend/src/components/header-footer/Header.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {BrowserRouter as router, Link} from 'react-router-dom';
 import Menu from '../buttons/Menu';
 
 const Header = () => {
